b2b1be6b45c05c676a50ddd74b2411ce4bac5d7a
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-12-26 15:51:40 -0500

    Pipe reverse - Done

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+
+  transform(value: string): any {
+    if (value.length) {
+      const charArray = [];
+      for (const i of value) {
+        charArray.push(i);
+      }
+      return charArray.reverse().join('');
+    } else {
+      return value;
+    }
+  }
+
+}
